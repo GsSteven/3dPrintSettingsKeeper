18f63fe33cf732343064fee2c2fb585893cd2aed
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-02-02 16:38:43 -0500

    added heroku production coding to server and json

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 8080;
@@ -34,6 +35,17 @@ app.use(cors());
 //route middleware
 app.use('/api/prints', prints);
 app.use('/api/upload', imageUploader);
+
+
+//for production app
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
